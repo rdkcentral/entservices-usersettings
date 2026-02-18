8727ef882be08cd212e4f340316491648c26f177
nnaveen979 <naveen.dunna057@gmail.com>
2026-02-18 07:57:38 +0530

    repo cleanup, making usersettings plugin as standalone repo

--- cov_build.sh
+++ cov_build.sh
@@ -5,11 +5,11 @@ set -e
 GITHUB_WORKSPACE="${PWD}"
 ls -la ${GITHUB_WORKSPACE}
 ############################
-# Build entservices-infra
-echo "buliding entservices-infra"
+# Build entservices-usersettings
+echo "building entservices-usersettings"
 
 cd ${GITHUB_WORKSPACE}
-cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-infra \
+cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-usersettings \
 -DUSE_THUNDER_R4=ON \
 -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
 -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
@@ -23,7 +23,6 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-infra \
 -DDS_FOUND=ON \
 -DPLUGIN_USBDEVICE=ON \
 -DPLUGIN_USB_MASS_STORAGE=ON \
--DPLUGIN_OCICONTAINER=ON \
 -DPLUGIN_TELEMETRY=ON \
 -DCMAKE_CXX_FLAGS="-DEXCEPTIONS_ENABLE=ON \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers \
@@ -64,6 +63,6 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-infra \
 -DHAS_RBUS -DDISABLE_SECURITY_TOKEN -DENABLE_DEVICE_MANUFACTURER_INFO -DUSE_THUNDER_R4=ON -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
 
 
-cmake --build build/entservices-infra --target install
+cmake --build build/entservices-usersettings --target install
 echo "======================================================================================"
 exit 0
