e9160660324570dd1770eff7c4fb26d8372f7189
agampa263 <133222558+agampa263@users.noreply.github.com>
2026-03-08 08:53:24 +0530

    RDKEMW-11928:Fix Coverity identified issues - entservices-infra (#9)

--- plugin/UserSettings.cpp
+++ plugin/UserSettings.cpp
@@ -48,7 +48,7 @@ namespace WPEFramework
      **/
     SERVICE_REGISTRATION(UserSettings, API_VERSION_NUMBER_MAJOR, API_VERSION_NUMBER_MINOR, API_VERSION_NUMBER_PATCH);
 
-    UserSettings::UserSettings() : _service(nullptr), _connectionId(0), _userSetting(nullptr), _userSettingsInspector(nullptr), _usersettingsNotification(this)
+    UserSettings::UserSettings() : _service(nullptr), _connectionId(0), _userSetting(nullptr), _userSettingsInspector(nullptr), _usersettingsNotification(this), configure(nullptr)
     {
         SYSLOG(Logging::Startup, (_T("UserSettings Constructor")));
     }
